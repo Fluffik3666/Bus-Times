92a0cf49f80bf694a6d1d8a588aba710b4dd1ec9
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2024-03-15 19:58:00 +0000

    Initial Commit

---
+++ Bus Times/Bus_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bus_TimesApp.swift
+//  Bus Times
+//
+//  Created by Sasha Bagrov on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bus_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
